4574acf63ff863cc0d17d49ea4f0d0b764e1034e
pc709 <pc709@iucesmag.edu.co>
2018-04-03 13:40:35 -0500

    Cambio 3

--- app/src/main/java/com/example/aula7/constraintlayout/Views/Main2Activity.java
+++ app/src/main/java/com/example/aula7/constraintlayout/Views/Main2Activity.java
@@ -1,23 +1,23 @@
-package com.example.aula7.constraintlayout;
+package com.example.aula7.constraintlayout.Views;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.aula7.constraintlayout.Views.Main2Activity;
+import com.example.aula7.constraintlayout.MainActivity;
+import com.example.aula7.constraintlayout.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
     public void NextWindow(View view){
-        Intent intent = new Intent(this, Main2Activity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
-
 }
